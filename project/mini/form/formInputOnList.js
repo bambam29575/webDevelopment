496848e9ff59bbb41a47871d4ad17c74922b1223
bambam29575 <123730907+bambam29575@users.noreply.github.com>
2024-07-17 16:34:57 +0530

    Update formInputOnList.js

--- project/mini/form/formInputOnList.js
+++ project/mini/form/formInputOnList.js
@@ -4,9 +4,9 @@ const animals = document.querySelector('#animals')
 
 formPost.addEventListener('submit', function(e) {
     e.preventDefault();
-    const animal = input.value;
-    const newLi = document.createElement('li');
-    newLi.innerText = animal;
-    animals.append(newLi);
-    input.value = "";
+    const animal = input.value; //stores value of input in animal variable
+    const newLi = document.createElement('li'); //new li created
+    newLi.innerText = animal; //li value is written according to animal value
+    animals.append(newLi); //Under animals ul , li is being added
+    input.value = ""; //Clears the input text area
 })
